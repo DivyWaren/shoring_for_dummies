0a3d9726a6168000fb4af771ad311fce1097f98b
Shafie <32918698+AhmadShafie@users.noreply.github.com>
2025-02-05 21:40:36 +0800

    updated numeric field to allow up to 2 decimal places

--- lib/src/common/numeric_field.dart
+++ lib/src/common/numeric_field.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Widget buildNumericField(String label, TextEditingController controller) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextField(
-        controller: controller,
-        keyboardType: TextInputType.number,
-        inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly], // Allows only digits [0 - 9] only
-        decoration: InputDecoration(
-          labelText: label,
-          border: OutlineInputBorder(),
-        ),
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: TextField(
+      controller: controller,
+      keyboardType: TextInputType.numberWithOptions(decimal: true), // Allows decimal input
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d{0,2}'))
+      ],
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(),
       ),
-    );
-  }
+    ),
+  );
+}
